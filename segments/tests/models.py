0ce12316c19f102b26cbf01fe318a1791e80d7b3
Igor Stroganov <ihar.strohanau@itechart-group.com>
2014-09-10 18:05:43 +0300

    Fix related names in test model

--- segments/tests/models.py
+++ segments/tests/models.py
@@ -1,4 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from segments.models import SegmentMixin
 
-class SegmentableUser(AbstractUser, SegmentMixin): pass
+class SegmentableUser(AbstractUser, SegmentMixin):
+    pass
+
+related_names_for = ('groups', 'user_permissions')
+for field_name in related_names_for:
+    field = SegmentableUser._meta.get_field_by_name(field_name)[0]
+    field.rel.related_name = '+'
+
