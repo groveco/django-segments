432d15ed2bc493e46dea608f79fe5bd431f6c139
Anton Shutik <anton.shutik@itechart-group.com>
2015-05-13 15:09:35 +0300

    Added initial migration

---
+++ segments/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Segment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=128)),
+                ('definition', models.TextField()),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='SegmentMembership',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('segment', models.ForeignKey(related_name='member_set', to='segments.Segment')),
+                ('user', models.ForeignKey(related_name='segment_set', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='segmentmembership',
+            unique_together=set([('user', 'segment')]),
+        ),
+    ]
