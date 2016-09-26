0139640e076d4ba745a1edccde8a5a1dd381efbd
Chris Clark <chris@untrod.com>
2016-09-26 08:08:46 -0700

    Django 1.10 compat

--- segments/tests/models.py
+++ segments/tests/models.py
@@ -6,6 +6,6 @@ class SegmentableUser(AbstractUser, SegmentMixin):
 
 related_names_for = ('groups', 'user_permissions')
 for field_name in related_names_for:
-    field = SegmentableUser._meta.get_field_by_name(field_name)[0]
+    field = SegmentableUser._meta.get_field(field_name)
     field.rel.related_name = '+'
 
