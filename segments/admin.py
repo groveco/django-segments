ea5b242e0cbd2470d3b3b71f5e2466cbb6129643
Kate Kligman <kate@katekligman.com>
2019-03-15 08:08:56 -0700

    PLAT-502 Respect async segment refresh in admin

--- segments/admin.py
+++ segments/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib import messages
 from segments.models import Segment
 from segments import app_settings
+from segments.tasks import refresh_segment
 
 
 class SegmentAdmin(admin.ModelAdmin):
@@ -20,8 +21,11 @@ class SegmentAdmin(admin.ModelAdmin):
 
     def refresh(self, request, queryset):
         for s in queryset:
-            s.refresh()
-        self.message_user(request, 'Refreshed %s segments.' % len(queryset))
+            if app_settings.SEGMENTS_REFRESH_ASYNC:
+                refresh_segment.delay(s.id)
+            else:
+                s.refresh()
+                self.message_user(request, 'Refreshed %s segments.' % len(queryset))
 
     def save_model(self, request, obj, form, change):
         if app_settings.SEGMENTS_REFRESH_ASYNC and (not change or app_settings.SEGMENTS_REFRESH_ON_SAVE):
