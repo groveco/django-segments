c84248cea6c6e6e3aa3141d3e0b5bc8165e17f5c
Kate Kligman <kate@katekligman.com>
2019-03-15 08:17:57 -0700

    PLAT-502 Use the segment helper for the user mixin

--- segments/models.py
+++ segments/models.py
@@ -135,7 +135,7 @@ class SegmentMixin(object):
     @property
     def segments(self):
         """Return all the segments to which this member belongs."""
-        return Segment.objects.filter(id__in=SegmentHelper().get_user_segments(self.pk)).order_by('-priority')
+        return Segment.objects.filter(id__in=Segment.helper.get_user_segments(self.pk)).order_by('-priority')
 
     def is_member(self, segment):
         """Helper method. Proxies to segment.has_member(self)"""
