66478f19cf02d36e51a256644200470254d54010
Kate Kligman <kate@katekligman.com>
2019-03-28 16:40:22 -0700

    PLAT-502 Modify segment refresh task to run parallel

--- segments/tasks.py
+++ segments/tasks.py
@@ -1,36 +1,22 @@
 from segments.models import Segment, SegmentExecutionError
-from django.contrib.auth import get_user_model
 from celery import task
-from time import time
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-@task(name='segments_refresh')
+@task(name='refresh_segments')
 def refresh_segments():
-    """Celery task to refresh all segments, with timing information. Writes to the logger."""
-    start = time()
-    failed = []
+    """Celery task to refresh all segments."""
     segments = list(Segment.objects.all())
     for s in segments:
-        start_seg = time()
-
         try:
-            s.refresh()
+            refresh_segment.delay(s.id)
         except SegmentExecutionError:
-            failed.append(s)
-
-        end_seg = time()
-        logger.info("SEGMENTS: Refreshed segment %s (id: %s) in %s milliseconds"
-                    % (s.name, s.id, (end_seg - start_seg) * 1000))
-
-    end = time()
-    logger.info("SEGMENTS: Successfully refreshed %s segments. Failed to refresh %s segments. Complete in %s seconds"
-                % (len(segments)-len(failed), len(failed), end - start))
+            logger.exception("SEGMENTS: Error refreshing segment id %s", s.id)
 
 
-@task(name='segment_refresh')
+@task(name='refresh_segment')
 def refresh_segment(segment_id):
     """Celery task to refresh an individual Segment."""
     try:
