409b3cd5dadd610cd779601b06e6853941590ade
Kate Kligman <kate@katekligman.com>
2019-03-29 09:36:51 -0700

    PLAT-502 Add renamable celery queue for tasks

--- segments/tasks.py
+++ segments/tasks.py
@@ -1,12 +1,13 @@
 import logging
 from celery import task
+from segments.app_settings import SEGMENTS_CELERY_QUEUE
 from segments.helpers import SegmentHelper
 from segments.models import Segment, SegmentExecutionError
 
 logger = logging.getLogger(__name__)
 
 
-@task(name='refresh_segments')
+@task(name='refresh_segments', queue=SEGMENTS_CELERY_QUEUE)
 def refresh_segments():
     """Celery task to refresh all segments."""
     segments = list(Segment.objects.all())
@@ -17,7 +18,7 @@ def refresh_segments():
             logger.exception("SEGMENTS: Error refreshing segment id %s", s.id)
 
 
-@task(name='refresh_segment')
+@task(name='refresh_segment', queue=SEGMENTS_CELERY_QUEUE)
 def refresh_segment(segment_id):
     """Celery task to refresh an individual Segment."""
     try:
@@ -27,7 +28,7 @@ def refresh_segment(segment_id):
         logger.exception("SEGMENTS: Unable to refresh segment id %s. DoesNotExist.", segment_id)
 
 
-@task(name='delete_segment')
+@task(name='delete_segment', queue=SEGMENTS_CELERY_QUEUE)
 def delete_segment(segment_id):
     """Celery task to delete an individual Segment from Redis """
     SegmentHelper().delete_segment(segment_id)
