b77e0d3fff991cd994c0e5025aef6d973de1f63b
Kate Kligman <kate@katekligman.com>
2019-04-16 00:31:09 -0700

    Remove task names

--- segments/tasks.py
+++ segments/tasks.py
@@ -7,7 +7,7 @@ from segments.models import Segment, SegmentExecutionError
 logger = logging.getLogger(__name__)
 
 
-@task(name='refresh_segments', queue=SEGMENTS_CELERY_QUEUE)
+@task(queue=SEGMENTS_CELERY_QUEUE)
 def refresh_segments():
     """Celery task to refresh all segments."""
     segments = list(Segment.objects.all())
@@ -18,7 +18,7 @@ def refresh_segments():
             logger.exception("SEGMENTS: Error refreshing segment id %s", s.id)
 
 
-@task(name='refresh_segment', queue=SEGMENTS_CELERY_QUEUE)
+@task(queue=SEGMENTS_CELERY_QUEUE)
 def refresh_segment(segment_id):
     """Celery task to refresh an individual Segment."""
     try:
@@ -28,7 +28,7 @@ def refresh_segment(segment_id):
         logger.exception("SEGMENTS: Unable to refresh segment id %s. DoesNotExist.", segment_id)
 
 
-@task(name='delete_segment', queue=SEGMENTS_CELERY_QUEUE)
+@task(queue=SEGMENTS_CELERY_QUEUE)
 def delete_segment(segment_id):
     """Celery task to delete an individual Segment from Redis """
     SegmentHelper().delete_segment(segment_id)
